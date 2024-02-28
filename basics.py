07cef480c57da38fd1f8a1e1947ff61197fb278e
empress28 <adamaigboempress@gmail.com>
2024-02-28 23:04:50 +0100

    pythonlesson

---
+++ basics.py
@@ -0,0 +1,31 @@
+# print("I am glad to be here at NUPAT TECHNOLOGIES")
+# Nupat ="This is a tech hub"
+# print(Nupat) 
+# Ulego ="This is a Banking App"
+# print(Ulego)
+ 
+#This is devops
+
+user_input_1 =input("what is your name: ") 
+user_input_2 =input("what is your age: ")
+user_input_3 =input("what is your gender: ") 
+
+print(user_input_1) 
+print(user_input_2) 
+print(user_input_3)
+
+
+print("my name is uje,", user_input_1) 
+print("my age is 23,", user_input_2)
+print("my gender is female,", user_input_3) 
+
+
+user_input_4 =input("who are you: ")
+user_input_5 =input("where are you: ")
+user_input_6 =input("what are you: ") 
+
+
+print(user_input_4)
+print(user_input_5)
+print(user_input_6) 
+
---
+++ operation.py
@@ -0,0 +1,74 @@
+#addition
+# number_1 =4
+
+# number_2 =6
+
+# print(number_1 + number_2)
+
+# number_sum=(number_1 + number_2)
+
+# print(number_sum)
+
+
+
+
+# subtraction 
+
+# number_3 =10
+
+# number_4 =5
+
+# number_subtraction= number_3 - number_4
+
+# print(number_subtraction) 
+
+
+
+
+# multiplication
+
+# number_6 =5
+
+# number_7 =10
+
+# number_multiplication = number_6 * number_7
+
+# print(number_multiplication)
+
+
+
+# division
+
+# number_8 =100
+
+# number_9 =25
+
+# number_division =number_8 / number_9
+
+# print(number_division) 
+ 
+
+# phythagoras theorem  
+
+
+# opposite =float(input("Enter the height of the triangle: ")) 
+
+# adjacent =float(input("Enter the base of the traingle: "))
+
+# hypotenuse =  (opposite**2 + adjacent**2) ** (1/2)
+
+
+# print(float(hypotenuse))
+
+
+
+# phythagoras theorem
+
+
+
+# opposite = int(input("Enter the height: "))
+# adjacent =int(input("Enter the base: "))
+
+# hypotenuse = (pow(opposite,2) + pow(adjacent,2)) **(1/2) 
+
+# print (hypotenuse)
